e85fbb2a80fc735ff38a17bda41d2ca8ce13a8dd
hqmxlyx <937425049@qq.com>
2021-01-05 15:29:50 +0800

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,7 +2,10 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("asdfd");
-		System.out.println("asdfd");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
 	}
 }
